38110592f06eef67401982fce6589cbd5aef3cb2
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-07-18 21:34:36 +0530

    use effect

---
+++ src/Components/EffectBasics.js
@@ -0,0 +1,26 @@
+import React,{useState, useEffect} from "react";
+
+const EffectBasics = () => {
+    let [a,setA] = useState(0);
+    let [b,setB] = useState(0);
+    let [c,setC] = useState(0);
+
+   useEffect(()=>{
+      console.log("useEffect called");
+      document.title = `A: ${a} B: ${b} C: ${c}`;
+      console.log("useEffect ended");
+   }, [a,b] // [] , [a] , no second parameter
+   )
+    
+
+    return(
+        <div>
+            <h1> A: {a} B: {b} C: {c} </h1>
+            <button onClick={() => setA(a+1)}>Increase A</button>
+            <button onClick={() => setB(b+1)}>Increase B</button>
+            <button onClick={() => setC(c+1)}>Increase C</button>
+        </div>
+    )
+}
+
+export default EffectBasics;
